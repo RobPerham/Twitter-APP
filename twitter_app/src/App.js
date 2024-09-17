da1217513c3d6399107e88437a64a65a18284643
RobPerham <robperham15@gmail.com>
2024-09-17 11:35:23 +0100

    start up

--- twitter_app/src/App.js
+++ twitter_app/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <h1>hello app</h1>
+
+      {/* sidebar*/}
+      <Sidebar />
+
+      {/* feed */}
+
+      {/* widgets */}
     </div>
   );
 }
